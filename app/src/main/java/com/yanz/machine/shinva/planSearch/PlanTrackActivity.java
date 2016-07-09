dc672ef56c6fffe6493faf88dd2c9d03db761b3e
yanz <yanzixiu2010@126.com>
2016-07-09 09:30:50 +0800

    添加dropDownMenu的lib

---
+++ app/src/main/java/com/yanz/machine/shinva/planSearch/PlanTrackActivity.java
@@ -0,0 +1,13 @@
+package com.yanz.machine.shinva.planSearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlanTrackActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_track);
+    }
+}
